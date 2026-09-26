88795098f4a2ce779dc7679845aff0f2c1df3411
Dipti Mishra <mishradipti087@gmail.com>
2026-09-26 19:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (19.47%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+
+        vector<int> dp(n+1, -1);
+
+        int ans = solve(n, 1, dp) + solve(n, 2, dp);
+        return ans;
+    }
+
+    int solve( int &n, int i, vector<int> &dp)
+    {
+
+        if(i==n)
+            return 1;
+
+        if(i>n)
+            return 0;
+
+        if(dp[i]!=-1)
+            return dp[i];
+
+        
+        dp[i] = solve(n, i+1, dp) + solve(n, i+2, dp);
+        return dp[i];
+    }
+};
